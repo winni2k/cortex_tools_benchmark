3b79112ad502258172475e1911ff3c75e5f30c46
Warren W. Kretzschmar <wkretzsch@gmail.com>
2018-03-10 16:46:27 +0100

    Add multiple color benchmark

--- benchmark/test_acceptance/test_traversal/test_pabies_cds.py
+++ benchmark/test_acceptance/test_traversal/test_pabies_cds.py
@@ -13,4 +13,33 @@ def test_442_base_unitig(benchmark, tmpdir):
                                            colors=[0],
                                            out=intermediate_graph)
     benchmark(cortexpy.__main__.main, [str(a) for a in traverse_args])
-    # print_traversal(intermediate_graph)
+
+
+def test_442_base_unitig_four_colors(benchmark, tmpdir):
+    intermediate_graph = tmpdir / 'traversal.pickle'
+    initial_kmer = 'ATGGAGGCCGTGAAATCTTTGGAGGATGTGTTCGGCATGAAGGGTGG'
+    traverse_args = build_traverse_command(graphs=[PABIES_CDS for _ in range(4)],
+                                           initial_kmers=initial_kmer,
+                                           colors=[0],
+                                           out=intermediate_graph)
+    benchmark(cortexpy.__main__.main, [str(a) for a in traverse_args])
+
+
+def test_442_base_unitig_sixteen_colors(benchmark, tmpdir):
+    intermediate_graph = tmpdir / 'traversal.pickle'
+    initial_kmer = 'ATGGAGGCCGTGAAATCTTTGGAGGATGTGTTCGGCATGAAGGGTGG'
+    traverse_args = build_traverse_command(graphs=[PABIES_CDS for _ in range(16)],
+                                           initial_kmers=initial_kmer,
+                                           colors=[0],
+                                           out=intermediate_graph)
+    benchmark(cortexpy.__main__.main, [str(a) for a in traverse_args])
+
+
+def test_442_base_unitig_sixtyfour_colors(benchmark, tmpdir):
+    intermediate_graph = tmpdir / 'traversal.pickle'
+    initial_kmer = 'ATGGAGGCCGTGAAATCTTTGGAGGATGTGTTCGGCATGAAGGGTGG'
+    traverse_args = build_traverse_command(graphs=[PABIES_CDS for _ in range(64)],
+                                           initial_kmers=initial_kmer,
+                                           colors=[0],
+                                           out=intermediate_graph)
+    benchmark(cortexpy.__main__.main, [str(a) for a in traverse_args])
