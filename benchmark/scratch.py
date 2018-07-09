2c8021d4fb4f0ac58da76f1b4c043bbe71840323
Warren W. Kretzschmar <wkretzsch@gmail.com>
2018-07-09 18:56:55 -0400

    WIP

--- benchmark/scratch.py
+++ benchmark/scratch.py
@@ -1,15 +1,23 @@
-LETTERS = list('ACGT')
+import cortexpy.__main__
+from Bio import SeqIO
+
+import os
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+from benchmark.commands import CortexpyCommandBuilder, MccortexCommandBuilder
+
+CHROM_GRAPH = 'fixtures/yeast/NC_001133.9.1kbp.ctx'
+CHROM_GRAPH3 = 'fixtures/yeast/NC_001133.9.c3.1kbp.ctx'
+CHROM_GRAPH_ALL = 'fixtures/yeast/NC_001133.9.1kbp.ctx'
+INITIAL_KMER = 'CCACACCACACCCACACACCCACACACCACACCACACACCACACCAC'
 
 
-def test_string_join(benchmark):
-    the_list = list(LETTERS[i % 4] for i in range(100))
-    benchmark(''.join, the_list)
+def test_traverse_1kbp_contig_one_color(benchmark):
+    print_args = CortexpyCommandBuilder().traverse(graphs=[CHROM_GRAPH],
+                                                   initial_contig=INITIAL_KMER)
 
+    def something():  # unnecessary function call
+        cortexpy.__main__.main([str(a) for a in print_args])
 
-def test_tuple_convert(benchmark):
-    the_list = list(LETTERS[i % 4] for i in range(100))
-    benchmark(tuple, the_list)
-
-def test_split(benchmark):
-    the_string = ''.join(LETTERS[i % 4] for i in range(100))
-    benchmark(list, the_string)
+    benchmark.pedantic(something, iterations=1, rounds=1, warmup_rounds=1)
