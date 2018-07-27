db2a3ea2364bd3b328e1bab39bfbe7bd0eee1a96
Warren W. Kretzschmar <wkretzsch@gmail.com>
2018-07-27 13:48:14 +0200

    Update for python3.7

--- benchmark/test_acceptance/test_view/test_yeast.py
+++ benchmark/test_acceptance/test_view/test_yeast.py
@@ -8,19 +8,17 @@ graph1 = 'fixtures/yeast/g750.traverse.ctx'
 graph2 = 'fixtures/yeast/g122.traverse.ctx'
 
 
-@pytest.mark.timeout(20)
 def test_view_traversal_3521_kmers(benchmark):
     traverse_args = CortexpyCommandBuilder().view_traversal(graph=graph0)
     benchmark(cortexpy.__main__.main, traverse_args)
 
 
-@pytest.mark.timeout(20)
 def test_view_traversal_12233_kmers(benchmark):
     traverse_args = CortexpyCommandBuilder().view_traversal(graph=graph1)
     benchmark(cortexpy.__main__.main, traverse_args)
 
 
-@pytest.mark.timeout(20)
+@pytest.mark.timeout(120)
 def test_view_traversal_26726_kmers(benchmark):
     traverse_args = CortexpyCommandBuilder().view_traversal(graph=graph2)
     benchmark(cortexpy.__main__.main, traverse_args)
