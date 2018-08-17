876a546042edef6a26976e4098405cef832fc570
Warren W. Kretzschmar <wkretzsch@gmail.com>
2018-08-17 17:23:19 +0200

    Add memory profiling

--- benchmark/test_unit/test_command/test_view/test_yeast_contig.py
+++ benchmark/test_unit/test_command/test_view/test_yeast_contig.py
@@ -1,12 +1,24 @@
+import pytest
+from benchmark.commands import CortexpyCommandBuilder
+
 import cortexpy.__main__
-import os
 
-from benchmark.commands import CortexpyCommandBuilder, MccortexCommandBuilder, CortexjdkCommandBuilder
+CHROM_GRAPH_1k = 'fixtures/yeast/NC_001133.9.1kbp.ctx'
+CHROM_GRAPH = 'fixtures/yeast/NC_001133.9.64kbp.ctx'
 
-CHROM_GRAPH = 'fixtures/yeast/NC_001133.9.1kbp.ctx'
 INITIAL_KMER = 'CCACACCACACCCACACACCCACACACCACACCACACACCACACCAC'
 
 
 def test_view_graph_1kbp_contig(benchmark):
-    print_args = CortexpyCommandBuilder().view_graph(graph=CHROM_GRAPH)
+    print_args = CortexpyCommandBuilder().view_graph(graph=CHROM_GRAPH_1k)
     benchmark(cortexpy.__main__.main, [str(a) for a in print_args])
+
+
+def test_view_traversal_1kbp_contig(benchmark):
+    print_args = CortexpyCommandBuilder().view_traversal(graph=CHROM_GRAPH_1k)
+    benchmark(cortexpy.__main__.main, [str(a) for a in print_args])
+
+
+def test_view_traversal_contig_memory():
+    print_args = CortexpyCommandBuilder().view_traversal(graph=CHROM_GRAPH)
+    cortexpy.__main__.main([str(a) for a in print_args])
